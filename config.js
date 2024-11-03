ee2d5a03c2bc4db976aa57abe98b8d0e8bcf6ead
ABASS4511 <sawadogoabdoul611@gmail.com>
2024-11-03 01:45:52 +0000

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
-global.botNumber = '2250500988391' //replace your bot number here
+global.botNumber = '2250501727398' //replace your bot number here
 
 global.mods = []
 global.prems = []
